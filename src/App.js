8d487da39f5ddc3ee3f98653068f2c0c95a50ffe
Aaron Janes <aaron.janes@gmail.com>
2021-11-07 06:49:55 -0700

    added form

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { ethers } from 'ethers'
 import { Warning } from './components/Alert'
-import { Button, Container, Row, Col, Spinner } from 'react-bootstrap'
+import { Button, Container, Row, Col, Spinner, Form } from 'react-bootstrap'
 import abi from './utils/WavePortal.json'
 // import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
@@ -9,8 +9,46 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 const App = () => {
   const [currentAccount, setCurrentAccount] = useState('')
   const [error, setError] = useState('')
-  const contractAddress = '0xd62aAC594D3f29E82b479Cd77707964a310E044d'
+  const [spinner, setSpinner] = useState(false)
+  const [allWaves, setAllWaves] = useState([])
+  const [message, setMessage] = useState({ username: '', message: '' })
+
+  const contractAddress = '0xB00eA5AE36e261ae313f97FFFa2459A79238e6e0'
   const contractABI = abi.abi
+
+  const getAllWaves = async () => {
+    try {
+      const { ethereum } = window
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum)
+        const signer = provider.getSigner()
+        const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer)
+
+        /*
+         * Call the getAllWaves method from your Smart Contract
+         */
+        const waves = await wavePortalContract.getAllWaves()
+
+        let wavesCleaned = []
+        waves.forEach((wave) => {
+          wavesCleaned.push({
+            address: wave.waver,
+            timestamp: new Date(wave.timestamp * 1000),
+            message: wave.message,
+            username: wave.username,
+          })
+        })
+
+        setAllWaves(wavesCleaned)
+      } else {
+        setError("Ethereum object doesn't exist!")
+        // console.log("Ethereum object doesn't exist!")
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   const checkIfWalletIsConnected = async () => {
     try {
       const { ethereum } = window
@@ -28,6 +66,7 @@ const App = () => {
         const account = accounts[0]
         console.log('Found an authorized account:', account)
         setCurrentAccount(account)
+        getAllWaves()
       } else {
         console.log('No authorized account found')
       }
@@ -58,7 +97,6 @@ const App = () => {
     checkIfWalletIsConnected()
   }, [])
 
-  const [spinner, setSpinner] = useState(false)
   const wave = async () => {
     try {
       const { ethereum } = window
@@ -67,18 +105,19 @@ const App = () => {
         const provider = new ethers.providers.Web3Provider(ethereum)
         const signer = provider.getSigner()
         const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer)
+        setSpinner(true)
 
         let count = await wavePortalContract.getTotalWaves()
         console.log('Retrieved total wave count...', count.toNumber())
 
-        const waveTxn = await wavePortalContract.wave()
+        const waveTxn = await wavePortalContract.wave(message.message, message.username)
         console.log('Mining...', waveTxn.hash)
         //  set loading spiner
-        setSpinner(true)
         await waveTxn.wait()
         console.log('Mined -- ', waveTxn.hash)
         //  set loading to false
         setSpinner(false)
+        setMessage({ username: '', message: '' })
         count = await wavePortalContract.getTotalWaves()
         console.log('Retrieved total wave count...', count.toNumber())
       } else {
@@ -89,27 +128,81 @@ const App = () => {
     }
   }
 
+  const handleChange = (e) => {
+    const { name, value } = e.target
+    setMessage((prev) => ({ ...prev, [name]: value }))
+    console.log(`message`, message)
+  }
+
   return (
-    <Container fluid className='m-2'>
+    <Container fluid='md' className='mt-4'>
       <Row>
-        <Col md={2}> </Col>
-        <Col md={8} className='header' style={{ textAlign: 'center' }}>
+        <Col className='m-8' md={12}>
+          <h1 style={{ color: 'white', textAlign: 'center' }}> ⧫ Ethereum Powered ⧫ </h1>
+        </Col>
+      </Row>
+
+      <Row>
+        <Col md={{ span: 8, offset: 2 }} className='header' style={{ textAlign: 'center' }}>
           <h2> 👋 Hey there! </h2> <h4>Im Aaron and React + Solidity is Amazing </h4>{' '}
           <h4>Wave at me and I might send you some Ether</h4>
           {error && <Warning error={error} />}
-          {spinner && <Spinner animation='border' variant='danger' />}
-          <Col>
-            {!currentAccount && (
-              <Button className=' btn  btn-block col-8' variant='info' onClick={connectWallet}>
-                Connect To Wallet
-              </Button>
-            )}
-            <Button className='btn btn-block col-8 ' variant='info' onClick={wave}>
-              Wave at Me
-            </Button>
-          </Col>
         </Col>
-        <Col md={2}> </Col>
+      </Row>
+
+      <Row>
+        <Col md={{ span: 8, offset: 2 }}>
+          <Form>
+            <Form.Group className='mb-3' controlId='formBasicEmail'>
+              <Form.Label style={{ color: 'white' }}>Enter a user name</Form.Label>
+              <Form.Control
+                type='text'
+                placeholder='User name'
+                value={message.username}
+                name='username'
+                onChange={handleChange}
+              />
+            </Form.Group>
+
+            <Form.Group className='mb-3' controlId='formBasicPassword'>
+              <Form.Label style={{ color: 'white' }}>Your Message</Form.Label>
+              <Form.Control
+                as='textarea'
+                rows={3}
+                placeholder='Enter your message'
+                name='message'
+                value={message.message}
+                onChange={handleChange}
+              />
+            </Form.Group>
+            <Col>
+              {!currentAccount && (
+                <Button className=' btn  btn-block col-12' variant='info' onClick={connectWallet}>
+                  Connect To Wallet
+                </Button>
+              )}
+              <Button className='btn btn-block col-12' variant='info' onClick={wave}>
+                {spinner && <Spinner role='status' size='md' animation='border' />}
+                <span>Send me a message </span>
+              </Button>
+            </Col>
+          </Form>
+        </Col>
+      </Row>
+
+      <Row>
+        <Col md={{ span: 8, offset: 2 }}>
+          {allWaves.map((wave, index) => {
+            return (
+              <div key={index} style={{ backgroundColor: 'OldLace', marginTop: '16px', padding: '8px' }}>
+                <div> Name: {wave.username} </div>
+                <div>Address: {wave.address}</div>
+                <div>Time: {wave.timestamp.toString()}</div>
+                <div>Message: {wave.message}</div>
+              </div>
+            )
+          })}
+        </Col>
       </Row>
     </Container>
   )
